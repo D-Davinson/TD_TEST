791af0c1230f7175e0b9587ffc1550133d639a4e
Davinson <doglasprincedavinson@gmail.com>
2024-02-28 13:58:50 +0100

    3 Test qui passent correctement

---
+++ Test.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import Mock
+from  Class import Pizza, CartePizzeria, CartePizzeriaException
+
+class TestCartePizzeria(unittest.TestCase):
+    def test_is_empty_true(self):
+        c = CartePizzeria()
+        self.assertTrue(c.is_empty())
+    def test_is_empty_false(self):
+        c = CartePizzeria()
+        c.add_pizza(Mock(spec=Pizza))
+        self.assertFalse(c.is_empty())
+    def test_nb_pizza(self):
+        c = CartePizzeria()
+        c.add_pizza(Mock(spec=Pizza))
+        c.add_pizza(Mock(spec=Pizza))
+        c.add_pizza(Mock(spec=Pizza))
+        c.add_pizza(Mock(spec=Pizza))
+        self.assertEqual(c.nb_pizzas(),4)
+
+
+__name__ == '__main__'
+unittest.main()
