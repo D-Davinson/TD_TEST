9c5f490c52527c915e9d77bb1c80fc25306bc409
Davinson <doglasprincedavinson@gmail.com>
2024-02-28 13:33:37 +0100

    Add Classes [EXO1]

---
+++ Class.py
@@ -0,0 +1,25 @@
+class CartePizzeriaException(Exception):
+    pass
+
+class Pizza:
+    def __init__(self, nom, ingredients, prix):
+        self.nom = nom
+        self.ingredients = ingredients
+        self.prix = prix
+
+class CartePizzeria:
+    def __init__(self):
+        self.pizzas = []
+    
+    def is_empty(self):
+        return len(self.pizzas) == 0
+    
+    def add_pizza(self,pizza):
+        return self.pizzas.append(pizza)
+    
+    def remove_pizza(self,name):
+        for pi in self.pizzas:
+            if pi.nom == name:
+                self.pizzas.remove(pi)
+                return
+            raise CartePizzeriaException(name)
